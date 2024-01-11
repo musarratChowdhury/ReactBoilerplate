0f212ac6efe61288fb6a918624f44e6261ccfd3e
musarratChowdhury <muhitlopez143@gmail.com>
2024-01-11 16:07:58 +0600

    many ui comps added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,18 @@
+import { Calendar } from "@/components/ui/calendar";
 import Wrapper from "@/components/wrapper";
+import React from "react";
 
 const Home = () => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
   return (
     <Wrapper>
       <h1>Home</h1>
+      <Calendar
+        mode="single"
+        selected={date}
+        onSelect={setDate}
+        className="rounded-md border"
+      ></Calendar>
     </Wrapper>
   );
 };
